a73ad527457a4e90193c77c7d90940292c4892ab
petitbon <87226+petitbon@users.noreply.github.com>
2025-04-14 16:02:21 -0400

    update

--- init.lua
+++ init.lua
@@ -221,7 +221,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
-vim.keymap.set('n', '<leader>ff', [[:%s@//.*@@g | %s@/\*\_.\{-}\*/@@g | %s/\n\(\s*\n\)\{2,}/\n\n/g<CR>]], { silent = true })
+vim.keymap.set('n', '<leader>ll', [[:%s/\(\n\s*\n\s*\)\+/\r\r/g<CR>]], { silent = true })
+vim.keymap.set('n', '<leader>cc', [[:%s@//.*@@g | %s@/\*\_.\{-}\*/@@g<CR>]], { silent = true })
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
