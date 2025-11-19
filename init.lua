190a4702a3887f3965fea3defe5241837518e363
petitbon <petitbon@gmail.com>
2025-11-19 06:44:42 -0500

    chore[PETITBON]: auto-commit

--- init.lua
+++ init.lua
@@ -985,10 +985,12 @@ require('lazy').setup({
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
+      -- cursor location to show total characters and LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
-        return '%2l:%-2v'
+        local wc = vim.fn.wordcount() or {}
+        local chars = wc.chars or 0
+        return 'chars:' .. chars .. ' ' .. '%2l:%-2v'
       end
 
       -- ... and there is more!
